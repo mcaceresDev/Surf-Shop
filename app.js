65b114960b5c52dbc22463b14bdd71f142b26e11
Manuel de Jesus Caceres Mercado <mcaceres@internal.inss.org>
2022-05-04 13:47:59 -0600

    preparando entorno para mongodb

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport')
+const session = require('express-session')
+const mongoose = require('mongoose')
+const User = require('./models/user')
 
+mongoose.connect('mongodb://localhost:27017/surf-shop')
+
+//require Routes
 const indexRouter   = require('./routes/index');
 const postsRouter   = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
-const usersRouter   = require('./routes/users');
+// const usersRouter   = require('./routes/users');
 
 const app = express();
 
@@ -21,8 +28,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Configure passport and sessions
+app.use(session({
+  secret: 'hang ten dude!',
+  resave: false,
+  saveUninitialized: true
+}))
+
+passport.use(User.createStrategy())
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+//Routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
 
